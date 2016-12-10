1e3642942d04987282b540a2ad5fd9f924504bb2
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2016-12-10 13:33:37 +0100

    Fix issue with merge

--- js/accordion.js
+++ js/accordion.js
@@ -2,7 +2,7 @@
  * Represents a simple accordion with transitions and max-height.
  *
  * @module Accordion
- * @version v2.0.3
+ * @version v2.0.6
  *
  * @author Sebastian Fitzner
  * @author Andy Gutsche
@@ -11,11 +11,11 @@
 /**
  * Requirements
  */
-import Helpers from '../../utils/helpers';
-import App from '../../app';
-import AppModule from '../_global/module';
+import App from 'app';
+import AppModule from 'app-module';
 
-var $ = App.$;
+const $ = App.$;
+const Helpers = App.Helpers;
 
 /**
  * Class Accordion
@@ -23,23 +23,26 @@ var $ = App.$;
 class Accordion extends AppModule {
 	constructor(obj) {
 		let options = {
-			openIndex: false,
-			openOnViewports: [
-				'desktop',
-				'tablet-large',
-				'tablet-small'
-			], // array: viewport names - eg.: ['mobile', 'tablet', 'desktop-small', 'desktop']
-			singleOpen: false,
 			activeClass: 'is-active',
-			openClass: 'is-open',
-			closeClass: 'is-closed',
-			calculatingClass: 'is-calculating',
-			unresolvedClass: 'is-unresolved',
-			removeStyles: false, // TODO
-			dataMaxAttr: 'data-js-height',
 			accordionBtn: '[data-js-atom="accordion-btn"]',
 			accordionContent: '[data-js-atom="accordion-content"]',
-			tabMode: false
+			calculatingClass: 'is-calculating',
+			clickHandler: 'click',
+			closeClass: 'is-closed',
+			dataMaxAttr: 'data-js-height',
+			openAllOnInit: false,
+			openByHash: false,
+			openClass: 'is-open',
+			openIndex: null,
+			openOnViewports: [
+				'tablet-small',
+				'tablet-large',
+				'desktop'
+			], // array: viewport names - eg.: ['mobile', 'tablet', 'desktop-small', 'desktop']
+			removeStyles: false, // TODO
+			singleOpen: false,
+			tabMode: false,
+			unresolvedClass: 'is-unresolved'
 		};
 
 		super(obj, options);
@@ -56,7 +59,7 @@ class Accordion extends AppModule {
 	static get info() {
 		return {
 			name: 'Accordion',
-			version: '2.0.3',
+			version: '2.0.6',
 			vc: true,
 			mod: false // set to true if source was modified in project
 		};
@@ -99,6 +102,16 @@ class Accordion extends AppModule {
 		this.$accordionBtns = $(this.options.accordionBtn, this.$el);
 		this.$target = null;
 		this.$btn = null;
+		this.openIndex = this.options.openIndex;
+
+		if (this.options.openByHash) {
+			let idx = this.getIndexByHash();
+
+			this.openIndex = typeof idx === 'number' ? idx : this.options.openIndex;
+		}
+		else if (this.options.tabMode && !this.options.openIndex) {
+			this.openIndex = 0;
+		}
 
 		// call super
 		super.initialize();
@@ -112,11 +125,15 @@ class Accordion extends AppModule {
 		let fnHandleClick = this.handleClick.bind(this);
 		let fnCloseAll = this.closeAll.bind(this);
 		let fnOpenAll = this.openAll.bind(this);
+		let fnOnHashChange = this.onHashChange.bind(this);
 
 		// Local events
-		this.$el.on('click touchstart', this.options.accordionBtn, fnHandleClick);
+		this.$el.on(this.options.clickHandler, this.options.accordionBtn, fnHandleClick);
 
 		// Global events
+		if (this.options.openByHash) {
+			$(window).on(App.EVENTS.hashchange, fnOnHashChange);
+		}
 		App.Vent.on(App.EVENTS.resize, fnRender);
 		App.Vent.on(App.EVENTS.accordion.closeAll, fnCloseAll);
 		App.Vent.on(App.EVENTS.accordion.openAll, fnOpenAll);
@@ -132,17 +149,64 @@ class Accordion extends AppModule {
 		this.saveHeights(this.$accordionContents);
 		this.closeAll();
 
-		// Open on index if set in options
-		if (typeof this.options.openIndex === 'number') {
+		if (this.options.openAllOnInit) {
+			this.openAll();
+		}
 
+		// Open on index if set in options
+		if (typeof this.openIndex === 'number') {
 			if (this.options.tabMode || this.options.openOnViewports.indexOf(App.currentMedia) !== -1) {
-				this.activateBtn(this.$accordionBtns.eq(this.options.openIndex));
-				this.slideDown(this.$accordionContents.eq(this.options.openIndex));
+				this.activateBtn(this.$accordionBtns.eq(this.openIndex));
+				this.slideDown(this.$accordionContents.eq(this.openIndex));
 			}
 		}
 
 		if (this.$el.hasClass(this.options.unresolvedClass)) {
 			this.$el.removeClass(this.options.unresolvedClass);
+		}
+	}
+
+	/**
+	 * Get index of accordion content referenced by hash
+	 *
+	 * @return {number|boolean} - index of element or false if no match
+	 */
+	getIndexByHash() {
+		let hash = document.location.hash.split('#');
+		let retVal = false;
+		let i = 0;
+
+		if (hash < 2) {
+			return false;
+		}
+
+		for (i; i < this.$accordionContents.length; i++) {
+			if (this.$accordionContents[i].id === hash[1]) {
+				retVal = i;
+				break;
+			}
+		}
+
+		return retVal;
+	}
+
+	/**
+	 * Open accordion content referenced by hash
+	 *
+	 * @param {object} e - event object
+	 */
+	onHashChange(e) {
+		let idx = this.getIndexByHash();
+
+		if (typeof idx === 'number') {
+
+			if (this.options.singleOpen) {
+				this.closeAll();
+			}
+
+			this.activateBtn(this.$accordionBtns.eq(idx));
+			this.slideDown(this.$accordionContents.eq(idx));
+
 		}
 	}
 
@@ -163,19 +227,19 @@ class Accordion extends AppModule {
 	 * @param {Object} item - item to calculate the height
 	 */
 	saveHeight(item) {
-		let el = item;
-		let wantedHeight = 0;
+		let $el = $(item);
+
 		// the el is hidden so:
 		// making the el block so we can measure its height but still be hidden
-		$(el).addClass(this.options.calculatingClass);
+		$el.addClass(this.options.calculatingClass);
 
-		wantedHeight = Helpers.getOuterHeight(el);
+		let wantedHeight = $el.outerHeight();
 
 		// reverting to the original values
-		$(el).removeClass(this.options.calculatingClass);
+		$el.removeClass(this.options.calculatingClass);
 
 		// save height in data attribute
-		el.setAttribute(this.options.dataMaxAttr, wantedHeight);
+		$el.attr(this.options.dataMaxAttr, wantedHeight);
 	}
 
 	/**
@@ -184,9 +248,10 @@ class Accordion extends AppModule {
 	 * execute the toggleContent method.
 	 *
 	 * @param {Object} e - event object
+	 * @param {object} currentTarget - Target to which listener was attached.
 	 */
-	handleClick(e) {
-		this.$btn = $(e.currentTarget);
+	handleClick(e, currentTarget) {
+		this.$btn = currentTarget ? $(currentTarget) : $(e.currentTarget);
 		let targetId = this.$btn.attr('href');
 
 		e.preventDefault();
@@ -228,13 +293,12 @@ class Accordion extends AppModule {
 	 * @param {Object} $item - jQuery object of item
 	 */
 	slideUp($item) {
-
 		$item
-				.css('height', 0)
-				.removeAttr('style')
-				.attr('aria-expanded', 'false')
-				.removeClass(this.options.openClass)
-				.addClass(this.options.closeClass);
+			.css('height', 0)
+			.removeAttr('style')
+			.attr('aria-expanded', 'false')
+			.removeClass(this.options.openClass)
+			.addClass(this.options.closeClass);
 	}
 
 	/**
@@ -243,12 +307,11 @@ class Accordion extends AppModule {
 	 * @param {Object} $item - jQuery object of item
 	 */
 	slideDown($item) {
-
 		$item
-				.css('height', $item.attr('data-js-height'))
-				.attr('aria-expanded', 'true')
-				.removeClass(this.options.closeClass)
-				.addClass(this.options.openClass);
+			.css('height', $item.attr('data-js-height') + 'px')
+			.attr('aria-expanded', 'true')
+			.removeClass(this.options.closeClass)
+			.addClass(this.options.openClass);
 	}
 
 	/**
