6157748a7adf33fb297d6d4fcf3633e0c02ae82b
Andy Gutsche <andy.gutsche@aperto.com>
2017-09-29 10:36:53 +0200

    Add settings json for component

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -2,7 +2,7 @@
  * Represents a simple accordion with transitions and max-height.
  *
  * @module Accordion
- * @version v5.0.0
+ * @version v5.1.0
  *
  * @author Sebastian Fitzner
  * @author Andy Gutsche
@@ -58,7 +58,7 @@ class Accordion extends VeamsComponent {
 	static get info() {
 		return {
 			name: 'Accordion',
-			version: '5.0.0',
+			version: '5.1.0',
 			vc: true,
 			mod: false // set to true if source was modified in project
 		};
